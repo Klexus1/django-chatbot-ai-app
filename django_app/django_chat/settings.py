841b210d8f3f71802a6b71e452fd4cb0c445af01
Klexus1 <47198827+Klexus1@users.noreply.github.com>
2023-12-30 16:18:37 +0100

    Upgrade of openai model

--- django_app/django_chat/settings.py
+++ django_app/django_chat/settings.py
@@ -138,3 +138,24 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "WARNING",
+    },
+    "loggers": {
+        "chat_app": {
+            "handlers": ["console"],
+            "level": os.getenv("DJANGO_LOG_LEVEL", "DEBUG"),
+            "propagate": False,
+        },
+    },
+}
